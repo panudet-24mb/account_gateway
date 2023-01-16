3244f1a9692f5ed498c4cb95d2cc9a128b55178f
panudet-24mb <panudet.panumas@gmail.com>
2023-01-16 15:28:44 +0700

    fix

--- microservice/internal/repository/users.go
+++ microservice/internal/repository/users.go
@@ -1,26 +1,25 @@
 package repository
 
 type User struct {
-	UserID         uint
-	UserName       string
-	HashPassword   string
-	Email          string
-	EmailConfirm   bool
-	UserStatus     string
-	LoginAttempt   int
-	AcceptTerms    bool
-	VerifyIdentity bool
-	LastLogin      string
-	IpAddress      string
-	RegisterAt     string
+	UserName       string `bson:"username"`
+	HashPassword   string `bson:"hashpassword"`
+	Email          string `bson:"email"`
+	EmailConfirm   bool   `bson:"emailconfirm"`
+	UserStatus     string `bson:"userstatus"`
+	LoginAttempt   int    `bson:"loginattemp"`
+	AcceptTerms    bool   `bson:"acceptterms"`
+	VerifyIdentity bool   `bson:"verifyidentity"`
+	LastLogin      string `bson:"lastlogin"`
+	IpAddress      string `bson:"ipaddress"`
+	RegisterAt     string `bson:"registerat"`
 }
 
 type UserRepository interface {
-	GetAll() ([]User, error)
-	GetById(int) (*User, error)
+	// GetAll() ([]User, error)
+	// GetById(int) (*User, error)
 	Create(User) (*User, error)
-	Update(User) (*User, error)
-	UpdateOne(User) (*User, error)
-	DeleteOne(int) error
-	FindUser(User) (*User, error)
+	// Update(User) (*User, error)
+	// UpdateOne(User) (*User, error)
+	// DeleteOne(int) error
+	// FindUser(User) (*User, error)
 }
--- microservice/internal/repository/users_db.go
+++ microservice/internal/repository/users_db.go
@@ -1,1 +1,27 @@
 package repository
+
+import (
+	"context"
+
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+type UserRepositoryDB struct {
+	db *mongo.Client
+}
+
+func NewUserRepositoryDB(db *mongo.Client) UserRepository {
+	return UserRepositoryDB{db: db}
+}
+
+func (r UserRepositoryDB) Create(u User) (*User, error) {
+	collection := r.db.Database("db").Collection("books")
+	doc := User{}
+	_, err := collection.InsertOne(context.TODO(), doc)
+	if err != nil {
+		return nil, err
+	}
+
+	return &u, err
+
+}
